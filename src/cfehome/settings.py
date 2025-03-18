852dbd304da4bdcfc2fcfa4ef116d74c31103149
Sankalp0030 <154550233+Sankalp0030@users.noreply.github.com>
2025-03-19 00:00:40 +0530

    changing the static root

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -142,19 +142,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
 STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
-STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR/"vendors"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
-#source(s) for python manage.py collectstatic
-STATICFILES_DIR = [
+# source(s) for python manage.py collectstatic 
+STATICFILES_DIRS = [
     STATICFILES_BASE_DIR
 ]
 
-# output(s) for python manage.py collectstatic
-#local cdn
+# output for python manage.py collectstatic 
+# local cdn
 STATIC_ROOT = BASE_DIR / "local-cdn"
+
+# < Django 4.2
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STORAGES = {
     "staticfiles": {
