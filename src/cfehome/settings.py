76fb0edb884908bd7e19a666d36b2b45a9cbb3a7
Sankalp0030 <154550233+Sankalp0030@users.noreply.github.com>
2025-03-29 03:01:03 +0530

    adding aullauth ui

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -18,15 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com" )
-EMAIL_PORT= config("EMAIL_PORT",default = "587")
-EMAIL_USE_TLS=config("EMAIL_USE_TLS",cast=bool,default = True)
-EMAIL_USE_SSL=config("EMAIL_USE_SSL",cast=bool,default = False)
-EMAIL_HOST_USER=config("EMAIL_HOST_USER", cast=str, default=None)
-EMAIL_HOST_PASSWORD=config("EMAIL_HOST_PASSWORD",cast=str,default = None)
-
-ADIMIN_USER_NAME=config("ADMIN_USER_NAME", default="Admin user")
+# Email config
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST", cast=str, default="smtp.gmail.com")
+EMAIL_PORT = config("EMAIL_PORT", cast=str, default="587") # Recommended
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)  # Use EMAIL_PORT 587 for TLS
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)  # Use MAIL_PORT 465 for SSL
+ADMIN_USER_NAME=config("ADMIN_USER_NAME", default="Admin user")
 ADMIN_USER_EMAIL=config("ADMIN_USER_EMAIL", default=None)
 
 MANAGERS=[]
@@ -67,9 +67,12 @@ INSTALLED_APPS = [
     'visits',
     'commando',
     #allauth
+    "allauth_ui",
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    "widget_tweaks",
+    "slippers",
 ]
 
 MIDDLEWARE = [
@@ -150,10 +153,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 #Django allauth config
 LOGIN_REDIRECT_URL = "/"
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_LOGIN_METHODS = {'email', 'username'}
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_EMAIL_SUBJECT_PREFIX = "[CFE]"
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
+
+
 AUTHENTICATION_BACKENDS = [
 
     # Needed to login by username in Django admin, regardless of `allauth`
